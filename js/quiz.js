c51c58eaaa75c0f4ecdf8757beabe2160cd7f39c
sudhanva <sudhanva.sa@gmail.com>
2018-04-30 12:12:37 +0530

    display image based question and selected index

--- js/quiz.js
+++ js/quiz.js
@@ -3,32 +3,30 @@ app.controller('quizCtlr',function($scope,quizMetrix,DataService){
 	$scope.activeQuestion = 0;
 	$scope.quizQuestions = DataService.quizQuestions;
 	$scope.numQuestionAnswered = 0;
-	//$scope.setActiveQuestion = setActiveQuestion;
-
-
 
 	$scope.questionAnswered =function(){
 		$scope.quizLength = $scope.quizQuestions.length;
-
 		if($scope.quizQuestions[$scope.activeQuestion].selected === null ){
 			$scope.numQuestionAnswered++;
-			
 			$scope.setActiveQuestion();
 			if($scope.numQuestionAnswered >= $scope.quizLength){
 			}
 		}
-		
 	}
-	$scope.setActiveQuestion = function(){
-			var breakOut=false;
-			var quizLength = $scope.quizQuestions.length-1;
 
-			while(!breakOut){
-				$scope.activeQuestion= $scope.activeQuestion < quizLength?++$scope.activeQuestion:0;
-				if($scope.quizQuestions[$scope.activeQuestion].selected === null){
-					breakOut = true;
-				}
+	$scope.setActiveQuestion = function(){
+		var breakOut=false;
+		var quizLength = $scope.quizQuestions.length-1;
+		while(!breakOut){
+			$scope.activeQuestion= $scope.activeQuestion < quizLength?++$scope.activeQuestion:0;
+			if($scope.quizQuestions[$scope.activeQuestion].selected === null){
+				breakOut = true;
 			}
 		}
+	}
+
+	$scope.selectedAnswer = function(index){
+		$scope.quizQuestions[$scope.activeQuestion].selected = index;
+	}
 
 });
